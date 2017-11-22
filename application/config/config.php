c1bef148f3c530555bd99404dd1dd5dc9a238563
syauqi fuadi <xfuadi@gmail.com>
2017-11-22 16:00:31 +0700

    change session name in config

--- application/config/config.php
+++ application/config/config.php
@@ -368,7 +368,7 @@ $config['encryption_key'] = '';
 |
 */
 $config['sess_driver'] = 'files';
-$config['sess_cookie_name'] = 'ci_session';
+$config['sess_cookie_name'] = 'arteri';
 $config['sess_expiration'] = 7200;
 $config['sess_save_path'] = NULL;
 $config['sess_match_ip'] = FALSE;
